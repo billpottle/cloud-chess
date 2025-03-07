db714525678779777cf9effc5f485a2bb4c19b2a
Bill Pottle <bill@kattaekwondo.com>
2025-03-07 13:03:33 -0700

    loading in progress multiplayer board

--- js/multiplayer.js
+++ js/multiplayer.js
@@ -4,6 +4,34 @@ let currentGameId = null;
 let playerColor = null;
 let isSpectatorMode = false;
 
+function convertPiecesToBoard(pieces) {
+    // Create empty 10x10 board
+    const board = Array(10).fill().map(() => Array(10).fill(''));
+    
+    // Map piece types to Unicode chess symbols
+    const pieceSymbols = {
+        'king': { 'white': '♔', 'black': '♚' },
+        'queen': { 'white': '♕', 'black': '♛' },
+        'rook': { 'white': '♖', 'black': '♜' },
+        'bishop': { 'white': '♗', 'black': '♝' },
+        'knight': { 'white': '♘', 'black': '♞' },
+        'pawn': { 'white': '♙', 'black': '♟' },
+        'archer': { 'white': '♙⇡', 'black': '♟⇣' },
+        'dragon': { 'white': 'dragon-white', 'black': 'dragon-black' }
+    };
+
+    // Place each piece on the board
+    pieces.forEach(piece => {
+        if (piece.type === 'dragon') {
+            board[piece.row][piece.col] = pieceSymbols.dragon[piece.color];
+        } else {
+            board[piece.row][piece.col] = pieceSymbols[piece.type][piece.color];
+        }
+    });
+
+    return board;
+}
+
 function initializeMultiplayerGame(gameId, color, currentTurn, boardState, isSpectator) {
     console.log('Initializing multiplayer game:', {
         gameId,
@@ -27,8 +55,8 @@ function initializeMultiplayerGame(gameId, color, currentTurn, boardState, isSpe
             console.log('Using initial board state');
             multiplayerGame.gameBoard = multiplayerGame.createInitialBoard();
         } else {
-            console.log('Using provided board state:', boardState);
-            multiplayerGame.gameBoard = boardState;
+            console.log('Converting provided board state');
+            multiplayerGame.gameBoard = convertPiecesToBoard(boardState);
         }
     }
 
