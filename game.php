1986815e1b4021ff253fb6d5981ac7e7dc5f82ed
Bill Pottle <bill@kattaekwondo.com>
2025-10-17 12:59:52 -0600

    codex styling

--- game.php
+++ game.php
@@ -45,7 +45,7 @@ ob_end_flush();
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Cloud Chess - Game #<?php echo $game_id; ?></title>
     
     <!-- CSS files -->
